184acb11a110d0947c18fca86b6a9995a303805c
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2022-07-29 13:04:34 -0300

    Add files via upload

---
+++ torch.lua
@@ -0,0 +1,36 @@
+require 'nn'
+require 'optim'
+require 'libunsup'
+
+-- extra modules
+torch.include('unsup', 'Diag.lua')
+-- classes that implement algorithms
+torch.include('unsup', 'UnsupModule.lua')
+torch.include('unsup', 'AutoEncoder.lua')
+torch.include('unsup', 'SparseAutoEncoder.lua')
+torch.include('unsup', 'FistaL1.lua')
+torch.include('unsup', 'LinearFistaL1.lua')
+torch.include('unsup', 'SpatialConvFistaL1.lua')
+torch.include('unsup', 'psd.lua')
+torch.include('unsup', 'LinearPsd.lua')
+torch.include('unsup', 'ConvPsd.lua')
+torch.include('unsup', 'UnsupTrainer.lua')
+torch.include('unsup', 'pca.lua')
+torch.include('unsup', 'kmeans.lua')
+torch.include('unsup', 'whitening.lua')
+
+local oldhessian = nn.hessian.enable
+function nn.hessian.enable()
+	oldhessian() -- enable Hessian usage
+	----------------------------------------------------------------------
+	-- Diag
+	----------------------------------------------------------------------
+	local accDiagHessianParameters = nn.hessian.accDiagHessianParameters
+	local updateDiagHessianInput = nn.hessian.updateDiagHessianInput
+	local updateDiagHessianInputPointWise = nn.hessian.updateDiagHessianInputPointWise
+	local initDiagHessianParameters = nn.hessian.initDiagHessianParameters
+
+	function nn.Diag.updateDiagHessianInput(self, input, diagHessianOutput)
+	   updateDiagHessianInput(self, input, diagHessianOutput, {'weight'}, {'weightSq'})
+	   return self.diagHessianInput
+	end
